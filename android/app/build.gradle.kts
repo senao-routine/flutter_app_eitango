c3e159cc0021c1ffffc7c73ef7f804e1ec72fc94
Shota Otobe <your.email@example.com>
2025-11-24 17:51:18 +0900

    chore: Application ID/Bundle IDをcom.routinelabo.koredakeに変更

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.english_vocab_app"
+    namespace = "com.routinelabo.koredake"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.english_vocab_app"
+        applicationId = "com.routinelabo.koredake"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
